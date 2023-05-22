7093c1a956c5dfa53174dfa0afb32c160e6d2342
Vincenarielm435 <131629321+Vincenarielm435@users.noreply.github.com>
2023-05-22 12:19:01 -0500

    Delete src/main/java/com/example/CrudSpringBoot directory

--- src/main/java/com/example/CrudSpringBoot/modelo/ConsultaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CrudSpringBoot.modelo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-
-}
